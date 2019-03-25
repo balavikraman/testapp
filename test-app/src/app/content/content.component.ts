62febddcacd183b74707c720d31e44ae631eaf86
Balavikraman <balavikraman@ideas2it.com>
2019-03-25 19:23:09 +0530

    commit

--- test-app/src/app/content/content.component.ts
+++ test-app/src/app/content/content.component.ts
@@ -13,7 +13,13 @@ export class ContentComponent implements OnInit {
     this.start();
   }
   buttonNumber=[];
+  options=[
+    { opt: "Option A" },
+    { opt: "Option B" },
+    { opt: "Option C" },
+    { opt: "Option D" },
 
+  ]
     start(){
       for(var i=1;i<=15;i++){
         this.buttonNumber.push(i)
