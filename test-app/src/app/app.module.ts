136af0d07e9e3dd6c7d878b661cdbd38565ddc3f
Balavikraman <vikram.g.p96@gmail.com>
2019-03-27 07:43:05 +0000

    app.module.ts edited online with Bitbucket

--- test-app/src/app/app.module.ts
+++ test-app/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -16,7 +16,8 @@ import { ContentComponent } from './content/content.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [{provide: LocationStrategy, useClass: HashLocationStrategy} ],
+  
   bootstrap: [AppComponent]
 })
 export class AppModule { }
