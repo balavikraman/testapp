52ee1e1a754b4184963347764a8046b4f4c9e2fc
Balavikraman <balavikraman@ideas2it.com>
2019-03-29 19:13:31 +0530

    questions navigation and modal popup finished

--- test-app/src/app/content/content.component.ts
+++ test-app/src/app/content/content.component.ts
@@ -8,23 +8,61 @@ import { Component, OnInit } from '@angular/core';
 export class ContentComponent implements OnInit {
 
   constructor() { }
-
+  public Questions;
+  public Options;
+  public i=0;
   ngOnInit() {
     this.start();
+    this.Questions = this.quesList[0];
+    this.Options = this.quesList[0].options;
   }
   buttonNumber=[];
-  options=[
-    { opt: "Option A" },
-    { opt: "Option B" },
-    { opt: "Option C" },
-    { opt: "Option D" },
+  quesList= [
+    {
+      Ques:"First Question ?",
+      options:[
+        "Option 1 A",
+        "Option 1 B",
+        "Option 1 C",
+        "Option 1 D",
+        
+      ]
+    },
+    {
+      Ques: "Second Question ?",
+      options: [
+        "Option 2 A",
+        "Option 2 B",
+        "Option 2 C",
+        "Option 2 D",
 
+      ]
+    },
+    {
+      Ques:"Third Question ?",
+      options:[
+         "Options 3 A",
+         "Options 3 B",
+         "Options 3 C",
+         "Options 3 D",
+        ]
+    }
   ]
-    start(){
+    start() {
       for(var i=1;i<=15;i++){
         this.buttonNumber.push(i)
       }
       console.log(this.buttonNumber);
       
+    }
+    prev() {
+      --this.i;
+      this.Questions = this.quesList[this.i];
+      this.Options = this.quesList[this.i].options;
+    }
+    next() {
+      ++this.i;
+      this.Questions = this.quesList[this.i];
+      this.Options = this.quesList[this.i].options;
     }
 }
